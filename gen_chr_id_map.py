7b11c50b3a2f759b652fe3b8cff0e05793519be1
durwa004 <durwa004@umn.edu>
2019-03-20 13:41:56 -0500

    Converts chr1 etc chromosomes back to NCBI

--- gen_chr_id_map.py
+++ gen_chr_id_map.py
@@ -5,50 +5,53 @@ Created on Tue Jan 29 19:11:48 2019
 
 @author: jonahcullen
 """
+#Get list of contig IDs from MSI vcf
+#import gzip
 
-id_map = {
-    'NC_009144.3':'chr1',  'NC_009145.3':'chr2',  'NC_009146.3':'chr3',
-    'NC_009147.3':'chr4',  'NC_009148.3':'chr5',  'NC_009149.3':'chr6',
-    'NC_009150.3':'chr7',  'NC_009151.3':'chr8',  'NC_009152.3':'chr9',
-    'NC_009153.3':'chr10', 'NC_009154.3':'chr11', 'NC_009155.3':'chr12',
-    'NC_009156.3':'chr13', 'NC_009157.3':'chr14', 'NC_009158.3':'chr15',
-    'NC_009159.3':'chr16', 'NC_009160.3':'chr17', 'NC_009161.3':'chr18',
-    'NC_009162.3':'chr19', 'NC_009163.3':'chr20', 'NC_009164.3':'chr21',
-    'NC_009165.3':'chr22', 'NC_009166.3':'chr23', 'NC_009167.3':'chr24',
-    'NC_009168.3':'chr25', 'NC_009169.3':'chr26', 'NC_009170.3':'chr27',
-    'NC_009171.3':'chr28', 'NC_009172.3':'chr29', 'NC_009173.3':'chr30',
-    'NC_009174.3':'chr31', 'NC_009175.3':'chrX',  'NC_001640.1':'chrMt'
-}
+#Read in contig IDs from MSI horses
+contig_list = list()
+#with gzip.open("/home/mccuem/shared/Projects/HorseGenomeProject/Data/EquCab3/interval_bio_comparison/MSI_run_files/interval_bio_10_cases.vcf.gz", "rt") as input_file:
+with open("/home/mccuem/shared/Projects/HorseGenomeProject/Data/EquCab3/interval_bio_comparison/MSI_run_files/interval_bio_10_cases.vcf", "r") as input_file:
+    for line in input_file:
+        if "#" in line:
+            if "##contig" in line:
+                contig,*_ = line.rstrip("\n").split(",")
+                a = contig.split("=")
+                contig_list.append(a[2])
+        else:
+            break
 
+#Need to read in IDs from NCBI file and then create a dictionary of chr:NC_
+id_map = {}
+with open("GCF_002863925.1_EquCab3.0_assembly_report.txt", "r") as input_file:
+    for line in input_file:
+        if "#" in line:
+            next
+        else:
+            nice,a,b,c,d,e,NCBI,*_ = line.rstrip("\n").split()
+            if "chr" in nice:
+                id_map[nice] = NCBI
+            else:
+                for i in range(len(contig_list)):
+                    if NCBI in contig_list[i]:
+                        id_map[contig_list[i]] = NCBI
 
+#Now want to read in vcf and change from id_map[key] to id_map[value]
+with open("/home/mccuem/shared/Projects/HorseGenomeProject/Data/EquCab3/interval_bio_comparison/MSI_run_files/interval_bio_10_cases.vcf", "r") as input_file:
+    with open("/home/mccuem/shared/Projects/HorseGenomeProject/Data/EquCab3/interval_bio_comparison/MSI_run_files/interval_bio_10_cases_copy.vcf", "w") as output_file:
+        for line in input_file:
+            if "##contig" in line:
+                contig,ID,chrom,*_ = line.rstrip("\n").split("=")
+                chrom = chrom.split(",")
+                chrom = chrom[0]
+                swap = id_map[chrom]
+                output_file.write(line.replace(chrom,swap))
+            elif "#" in line:
+                output_file.write(line)
+            elif "chr" in line:
+                chrom,*_ = line.rstrip("\n").split()
+                swap = id_map[chrom]
+                output_file.write(line.replace(chrom,swap)) 
+            else:
+                output_file.write(line)
 
-ensem_id_map = {}
-
-def inc_range(start, end):
-    return range(start, end+1)
-
-for i in inc_range(1,31):
-    ensem_id_map[str(i)] = 'chr' + str(i)
-    
-    if 'MT' or 'X' not in ensem_id_map:
-        ensem_id_map['MT'] = 'chrMt'
-        ensem_id_map['X'] = 'chrX'
-
-
-def gen_ensem_id_map():
-    
-    ensem_id_map = {}
-    
-    def inc_range(start, end):
-        return range(start, end+1)
-    
-    for i in inc_range(1,31):
-        ensem_id_map[str(i)] = 'chr' + str(i)
-        
-        if 'MT' or 'X' not in ensem_id_map:
-            ensem_id_map['MT'] = 'chrMt'
-            ensem_id_map['X'] = 'chrX'
-       
-    return ensem_id_map        
-
-test = gen_ensem_id_map()
